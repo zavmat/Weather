4980511496705a6a766f919da900bff5cf6d8714
Ivan Moore <ivan@teamoptimization.com>
2017-07-13 20:59:44 +0100

    added another forecaster library

--- src/main/java/com/develogical/Example.java
+++ src/main/java/com/develogical/Example.java
@@ -1,5 +1,6 @@
 package com.develogical;
 
+import com.oocode.weather.DayForecaster;
 import com.weather.Day;
 import com.weather.Forecast;
 import com.weather.Forecaster;
@@ -7,7 +8,11 @@ import com.weather.Region;
 
 public class Example {
     public static void main(String[] args) {
-        // This is just an example of using the 3rd party API - delete this class before submission.
+        lib1();
+        lib2();
+    }
+
+    private static void lib1() {
         Forecaster forecaster = new Forecaster();
 
         Forecast londonForecast = forecaster.forecastFor(Region.LONDON, Day.MONDAY);
@@ -19,5 +24,19 @@ public class Example {
 
         System.out.println("Edinburgh outlook: " + edinburghForecast.summary());
         System.out.println("Edinburgh temperature: " + edinburghForecast.temperature());
+    }
+
+    private static void lib2() {
+        Double londonForecastTemp = DayForecaster.forecast(com.oocode.weather.Forecaster.Regions.SE_ENGLAND, 0);
+        String londonForecastSummary = DayForecaster.forecastSummary(com.oocode.weather.Forecaster.Regions.SE_ENGLAND, 0);
+
+        System.out.println("London outlook: " + londonForecastSummary);
+        System.out.println("London temperature: " + londonForecastTemp);
+
+        Double edinburghForecastTemp = DayForecaster.forecast(com.oocode.weather.Forecaster.Regions.SE_SCOTLAND, 0);
+        String edinburghForecastSummary = DayForecaster.forecastSummary(com.oocode.weather.Forecaster.Regions.SE_SCOTLAND, 0);
+
+        System.out.println("Edinburgh outlook: " + edinburghForecastSummary);
+        System.out.println("Edinburgh temperature: " + edinburghForecastTemp);
     }
 }
