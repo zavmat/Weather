c23dd6839ba0e9076259e120cd09d286918823d8
zavmat <zavarzinm@gmail.com>
2020-02-19 17:23:36 +0000

    Test 1-4 Pass OK

--- src/main/java/com/develogical/CachingForecastClient.java
+++ src/main/java/com/develogical/CachingForecastClient.java
@@ -5,23 +5,30 @@ import com.weather.Forecast;
 import com.weather.Region;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CachingForecastClient {
     private final ForecasterClient delegate;
-    private Forecast cachedItem;
-    HashMap<Region, Forecast> localstorage = new HashMap<Region, Forecast>();
+    HashMap<Region, HashMap<Day, Forecast>> localStorage = new HashMap<Region, HashMap<Day, Forecast>> ();
     public CachingForecastClient(ForecasterClient delegate) {
         this.delegate = delegate;
     }
 
     public Forecast forecastFor(Region region, Day day) {
-        Forecast result=localstorage.get(region);
-        if (result != null) {
-            return result;
+        Forecast result;
+        HashMap<Day, Forecast> days= localStorage.get(region);
+        if (days != null) {
+            result = days.get(day);
+            if(result!=null){
+                return result;
+            }
+
         }
 
         result = delegate.forecastFor(region, day);
-        localstorage.put(region,result);
+        HashMap<Day,Forecast> element = new HashMap<Day,Forecast>();
+        element.put(day,result);
+        localStorage.put(region, element);
         return result;
     }
 }
--- src/test/java/com/develogical/PutYourTestCodeInThisDirectoryTest.java
+++ src/test/java/com/develogical/PutYourTestCodeInThisDirectoryTest.java
@@ -32,7 +32,7 @@ public class PutYourTestCodeInThisDirectoryTest {
     }
 
     @Test
-    public void testCacheWithDifferentQueries() throws Exception {
+    public void testCacheWithDifferentRegions() throws Exception {
         ForecasterClient delegate = mock(ForecasterClient.class);
         when(delegate.forecastFor(Region.LONDON, Day.MONDAY)).thenReturn(new Forecast("cloudy", 25));
         when(delegate.forecastFor(Region.BIRMINGHAM, Day.MONDAY)).thenReturn(new Forecast("sunny", 10));
@@ -44,4 +44,19 @@ public class PutYourTestCodeInThisDirectoryTest {
         assertThat(thirdForecast.summary(), equalTo("sunny"));
         assertThat(thirdForecast.temperature(), equalTo(10));
     }
+
+    @Test
+    public void testCacheWithDifferentDays() throws Exception {
+        ForecasterClient delegate = mock(ForecasterClient.class);
+        when(delegate.forecastFor(Region.LONDON, Day.MONDAY)).thenReturn(new Forecast("cloudy", 25));
+        when(delegate.forecastFor(Region.LONDON, Day.TUESDAY)).thenReturn(new Forecast("sunny", 10));
+        CachingForecastClient client = new CachingForecastClient(delegate);
+        Forecast newForecast = client.forecastFor(Region.LONDON, Day.MONDAY);
+        Forecast secondForecast = client.forecastFor(Region.LONDON, Day.TUESDAY);
+        Forecast thirdForecast = client.forecastFor(Region.LONDON, Day.TUESDAY);
+        verify(delegate, times(2)).forecastFor(ArgumentMatchers.<Region>any(), ArgumentMatchers.<Day>any());
+        assertThat(thirdForecast.summary(), equalTo("sunny"));
+        assertThat(thirdForecast.temperature(), equalTo(10));
+    }
+
 }
