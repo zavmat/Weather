5836b2ee86d9db336944e72dfc0e096ae8c27cc6
zavmat <zavarzinm@gmail.com>
2020-02-19 16:43:56 +0000

    Test 1+2 Pass

--- src/main/java/com/develogical/CachingForecastClient.java
+++ src/main/java/com/develogical/CachingForecastClient.java
@@ -5,16 +5,19 @@ import com.weather.Forecast;
 import com.weather.Region;
 
 public class CachingForecastClient {
-
     private final ForecasterClient delegate;
+    private Forecast cachedItem;
 
     public CachingForecastClient(ForecasterClient delegate) {
-            this.delegate=delegate;
+        this.delegate = delegate;
     }
 
-    public  Forecast forecastFor(Region region, Day day) {
-
+    public Forecast forecastFor(Region region, Day day) {
+        if (cachedItem != null) {
+            return cachedItem;
+        }
         Forecast cached = delegate.forecastFor(region, day);
+        this.cachedItem = cached;
         return cached;
     }
 }
--- src/test/java/com/develogical/PutYourTestCodeInThisDirectoryTest.java
+++ src/test/java/com/develogical/PutYourTestCodeInThisDirectoryTest.java
@@ -11,23 +11,23 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.mockito.Mockito.*;
 
 public class PutYourTestCodeInThisDirectoryTest {
-
     @Test
     public void TestQueryForForecast() throws Exception {
         ForecasterClient delegate = mock(ForecasterClient.class);
         when(delegate.forecastFor(Region.LONDON, Day.MONDAY)).thenReturn(new Forecast("cloudy", 25));
         CachingForecastClient client = new CachingForecastClient(delegate);
-        Forecast newForecast =  client.forecastFor(Region.LONDON, Day.MONDAY);
-        assertThat( newForecast.summary(), equalTo("cloudy"));
-        assertThat( newForecast.temperature(), equalTo(25));
-}
+        Forecast newForecast = client.forecastFor(Region.LONDON, Day.MONDAY);
+        assertThat(newForecast.summary(), equalTo("cloudy"));
+        assertThat(newForecast.temperature(), equalTo(25));
+    }
+
     @Test
     public void TestQueryWithCache() throws Exception {
         ForecasterClient delegate = mock(ForecasterClient.class);
         when(delegate.forecastFor(Region.LONDON, Day.MONDAY)).thenReturn(new Forecast("cloudy", 25));
         CachingForecastClient client = new CachingForecastClient(delegate);
-        Forecast newForecast =  client.forecastFor(Region.LONDON, Day.MONDAY);
-        Forecast sameForecast =  client.forecastFor(Region.LONDON, Day.MONDAY);
-        verify(delegate, times(1)).forecastFor(ArgumentMatchers.<Region>any(),ArgumentMatchers.<Day>any());
+        Forecast newForecast = client.forecastFor(Region.LONDON, Day.MONDAY);
+        Forecast sameForecast = client.forecastFor(Region.LONDON, Day.MONDAY);
+        verify(delegate, times(1)).forecastFor(ArgumentMatchers.<Region>any(), ArgumentMatchers.<Day>any());
     }
 }
